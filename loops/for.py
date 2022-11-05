768e1cf51e7b35f81c7d4bdc1eaf9582f91bb643
Tuncay Kayaoglu <64835817+tunc-ay@users.noreply.github.com>
2022-11-05 09:55:47 +0100

    create for loop for Udemy

---
+++ loops/for.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3,4,5,6,7]
+
+for i in numbers:
+    print("Hello world!")
+
+jedis = ["Obiwan", "Yoda", "Luke"]
+for jedi in jedis:
+    print("May be the force with you, " + jedi)
+
