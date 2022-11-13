80691f4e122ab2fd7d93541b030e9407c119ba39
Tuncay Kayaoglu <64835817+tunc-ay@users.noreply.github.com>
2022-11-13 13:29:46 +0100

    Create list-comprehension2.py

---
+++ loops/list-comprehension2.py
@@ -0,0 +1,14 @@
+# numbers = [1,2,3,6,8,10,21,44]
+
+# newNumbers = [i for i in numbers if i%2==0]       # You can write conditionals like this in comphre.
+
+# newNumbers2 = [i if i%2==0 else "NO" for i in numbers]
+
+# print(newNumbers)
+# print(newNumbers2)
+
+
+prodPrice = [1000, 2000, 3500, 0, 4000]
+taxedPrice = [i*1.18 for i in prodPrice if i > 0]
+print(taxedPrice)
+
