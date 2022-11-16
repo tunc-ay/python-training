5e6a827b2bd96886d97c2e924d0595e9d7304b51
Tuncay Kayaoglu <64835817+tunc-ay@users.noreply.github.com>
2022-11-16 19:45:04 +0100

    Create functions.py

---
+++ functions/functions.py
@@ -0,0 +1,12 @@
+def greetings():
+    print("Hello")
+greetings()
+
+for i in range(3):
+    greetings()
+
+def adding():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter a number: "))
+    print(a+b)
+adding()
